a776e065d63aa46922b0e776a76201224278f44e
kenziebourn <bournm@oregonstate.edu>
2022-11-02 11:30:33 -0700

    As of 11/2/22: 1. Still working on data extraction to CSV file 2. Still need unit tests

---
+++ test.py
@@ -0,0 +1,12 @@
+import csv
+
+# data to be written row-wise in csv file
+data = [['Geeks'], [4], ['geeks !']]
+
+# opening the csv file in 'w+' mode
+file = open('g4g.csv', 'w+', newline='')
+
+# writing the data into the file
+with file:
+    write = csv.writer(file)
+    write.writerows(data)
